d3f834bf877ccf61d4552404ac46724f373f0377
webmaitr <66726660+webmaitr@users.noreply.github.com>
2024-08-05 14:35:35 +0200

    Update index.js

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -1,3 +1,9 @@
 //votre code ici
-
+function printNumbers (n) {
+  let myNumbers = "";
+  for (let i=1; i<=n; i++) {
+    myNumbers += "n ";
+    return myNumbers;
+  }
+}
 export default printNumbers
