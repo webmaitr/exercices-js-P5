a60b39983e72fc6d60c1de03c30de4e985556aa1
webmaitr <66726660+webmaitr@users.noreply.github.com>
2024-08-05 14:34:50 +0200

    Update script.js

--- Exercice9/script.js
+++ Exercice9/script.js
@@ -1,3 +1,11 @@
 // votre code ici
-
+function checkAge (age) {
+  if (age < 18) {
+    console.log("vous êtes mineur.");
+  } else if (age >= 18 && age <=65) {
+    console.log("vous êtes majeur.");
+  } else {
+    console.log("vous êtes senior.");
+  }
+}
 export default checkAge;
